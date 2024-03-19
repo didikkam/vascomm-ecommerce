13749eceefde6281667dff24fc59b9ec6316a056
Didik Am <didik.abdul2017@gmail.com>
2024-03-19 11:16:58 +0700

    admin navbar

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -1,3 +1,31 @@
+<style>
+    .admin-box {
+        display: flex;
+        align-items: center;
+        padding: 8px 12px;
+        border-radius: 6px;
+    }
+
+    .admin-img {
+        width: 45px;
+        height: 45px;
+        border-radius: 50%;
+        overflow: hidden;
+        margin-right: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .admin-img img {
+        width: 100%;
+        height: auto;
+    }
+
+    .profile.dropdown-toggle::after {
+        display: none;
+    }
+</style>
 <nav class="navbar navbar-expand-lg navbar-light bg-white d-none d-md-block">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('index') }}">
@@ -8,20 +36,33 @@
             </ul>
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
-                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    <a class="nav-link active profile dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
-                        Administrator
+                        <div class="admin-box">
+                            <div class="me-3">
+                                <p class="py-0 my-0 text-end" style="color: #41A0E4; font-size: 13px;">Hallo Admin,</p>
+                                <p class="py-0 my-0 text-end">Didik Abdul Mukmin</p>
+                            </div>
+                            <div class="admin-img">
+                                <img src="https://via.placeholder.com/200" alt="Admin">
+                            </div>
+                        </div>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li>
-                            <span class="dropdown-item">
-                                <p class="my-0 py-0">email@mail.com</p>
-                            </span>
+                        <li class="px-5 py-3">
+                            <div class="d-flex justify-content-center text-center w-100">
+                                <div class="admin-img" style="height: 100px;width:100px;">
+                                    <img src="https://via.placeholder.com/200" alt="Admin">
+                                </div>
+                            </div>
+                            <p class="py-0 my-0 text-center mt-3">Didik Abdul Mukmin</p>
+                            <p class="py-0 my-0 text-center">email@mail.com</p>
                         </li>
                         <li>
-                            <hr class="dropdown-divider" />
-                        <li>
-                            <a class="dropdown-item" href="#">Logout</a>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li class="px-5 py-3 text-center">
+                            <a href="#" style="text-decoration: none;" class="text-danger"><img src="{{ asset('assets/images/power.png') }}" alt="Logo" height="20"> Keluar</a>
                         </li>
                     </ul>
                 </li>
--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -1,47 +1,53 @@
 <style>
-    ul.nav,
-    .nav-item {
-        width: 100%;
-    }
+    .sidebar {
 
-    .nav-item {
-        padding: 1px 0;
-    }
+        ul.nav,
+        .nav-item {
+            width: 100%;
+        }
 
-    .nav-link {
-        padding: 10px 10px;
-        color: #1A1111;
-    }
+        .nav-item {
+            padding: 1px 0;
+        }
 
-    .nav-item.active .nav-link,
-    .nav-item:hover .nav-link {
-        background-color: #41A0E4 !important;
-    }
+        .nav-link {
+            padding: 10px 10px;
+            color: #1A1111;
+        }
 
-    .nav-item.active a span,
-    .nav-item:hover a span {
-        color: #FFFFFF;
+        .nav-item.active .nav-link,
+        .nav-item:hover .nav-link {
+            background-color: #41A0E4 !important;
+        }
+
+        .nav-item.active a span,
+        .nav-item:hover a span {
+            color: #FFFFFF;
+        }
     }
 </style>
-<div class="col-auto col-md-3 col-xl-2 px-0 bg-white mt-1">
+<div class="sidebar col-auto col-md-3 col-xl-2 px-0 bg-white mt-1">
     <div class="d-flex flex-column align-items-center align-items-sm-start pt-2 text-white min-vh-100">
         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu"
             style="">
             <li class="nav-item {{ Route::is('admin.index') ? 'active' : '' }}">
                 <a href="{{ route('admin.index') }}" class="nav-link align-middle">
-                    <img src="{{ Route::is('admin.index') ? asset('assets/images/ic_dashboard_active.png') : asset('assets/images/ic_dashboard.png') }}" alt="Logo" height="25">
+                    <img src="{{ Route::is('admin.index') ? asset('assets/images/ic_dashboard_active.png') : asset('assets/images/ic_dashboard.png') }}"
+                        alt="Logo" height="25">
                     <span class="ms-1 d-none d-sm-inline">Dashboard</span>
                 </a>
             </li>
             <li class="nav-item {{ Route::is('admin.users.*') ? 'active' : '' }}">
                 <a href="{{ route('admin.users.index') }}" class="nav-link align-middle">
-                    <img src="{{ Route::is('admin.users.*') ? asset('assets/images/ic_user_active.png') : asset('assets/images/ic_user.png') }}" alt="Logo" height="25">
+                    <img src="{{ Route::is('admin.users.*') ? asset('assets/images/ic_user_active.png') : asset('assets/images/ic_user.png') }}"
+                        alt="Logo" height="25">
                     <span class="ms-1 d-none d-sm-inline">Manajemen User</span>
                 </a>
             </li>
             <li class="nav-item {{ Route::is('admin.products.*') ? 'active' : '' }}">
                 <a href="{{ route('admin.products.index') }}" class="nav-link align-middle">
-                    <img src="{{ Route::is('admin.products.*') ? asset('assets/images/ic_product_active.png') : asset('assets/images/ic_product.png') }}" alt="Logo" height="25">
+                    <img src="{{ Route::is('admin.products.*') ? asset('assets/images/ic_product_active.png') : asset('assets/images/ic_product.png') }}"
+                        alt="Logo" height="25">
                     <span class="ms-1 d-none d-sm-inline">Manajemen Produk</span>
                 </a>
             </li>
